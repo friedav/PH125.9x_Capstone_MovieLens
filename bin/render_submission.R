9ccfbc62e0123e14d5b006574d65110723401db1
Friederike David <fdavid@uni-bonn.de>
2020-08-05 12:07:21 +0200

    Remove unnecessary parameters

--- bin/render_submission.R
+++ bin/render_submission.R
@@ -8,13 +8,10 @@ library(here)
 library(rmarkdown)
 library(knitr)
 
-# render PDF report for submission
-render(input = here("submission/submission.Rmd"),
-       output_file = "submission.pdf",
-       output_dir = here("submission"),
-       knit_root_dir = here("submission"))
-
 # extract R code as base for submission.R (will be further modified)
 purl(input = here("submission/submission.Rmd"),
      output = here("submission/submission.R"),
      documentation = 2)
+
+# render PDF report for submission
+render(input = here("submission/submission.Rmd"))
